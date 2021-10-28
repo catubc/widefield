2a3d2b78e61ed5e32e2a9fad5a69f19738904960
cat <cat>
2021-10-28 18:17:38 +0200

    updates

--- yueqi/utils/utils.py
+++ yueqi/utils/utils.py
@@ -13,6 +13,16 @@ def load_data_PCA(fname_whole_stack,
                  fname_lever_times,
                  fname_blue_light):
 
+    '''
+    Function takes in names of lever triggers, blue light and whole stack and
+    returns the continuos as well as triggered segments
+
+    *** NOTE: this function has been superseeded by the next one that loads ROI based data from
+    whole stack locaNMF
+
+
+    '''
+
     # this is the entire session PCA stack
     d1 = np.load(fname_whole_stack)
     
@@ -45,9 +55,21 @@ def load_data_PCA(fname_whole_stack,
 
 
 ###################################################
-def load_data_ROIs(fname_whole_stack,
-                   fname_lever_times,
-                   fname_blue_light):
+def load_data_ROIs_lever_pulls(fname_whole_stack,
+                           fname_lever_times,
+                           fname_blue_light):
+
+    '''
+    Function takes as input the whole stack locaNMF data, lever trigger file names and blue-light shift
+
+    Exports:
+        stack: trigger centred segments of locaNMF data
+        stack_random: same as above but for random sampled trigger times
+        continuous:  locaNMF processed data shifted to match blue light shift
+        times: the trigger times;
+        times_nonrewarded: the
+    '''
+
 
     # this is the entire session PCA stack
     dall = np.load(fname_whole_stack)
@@ -78,22 +100,22 @@ def load_data_ROIs(fname_whole_stack,
     # make a time triggered stack to visualize there is indeed a peak in the correct place
     stack = []
     stack_random = []
+    min_lockout = 30*3  # min 3 seconds away from each trigger
     for t in times:
         temp = d1[t-900:t+900]
         if len(temp)==1800:
             stack.append(temp)
 
-        min_lockout = 30*3  # # of seconds away from
+            # grab random segments also at the same time;
+            while True:
+                t_random = np.random.randint(1000,38000)
+                min_diff = np.min(np.abs(times-t_random))
+                if min_diff>min_lockout:
 
-        while True:
-            t_random = np.random.randint(1000,38000)
-            min_diff = np.min(np.abs(times-t_random))
-            if min_diff>min_lockout:
-
-                temp2 = d1[t_random-900:t_random+900]
-                if len(temp2)==1800:
-                    stack_random.append(temp2)
-                break
+                    temp2 = d1[t_random-900:t_random+900]
+                    if len(temp2)==1800:
+                        stack_random.append(temp2)
+                    break
     #
     stack = np.array(stack)
     stack_random = np.array(stack_random)
@@ -102,6 +124,161 @@ def load_data_ROIs(fname_whole_stack,
     return stack, stack_random, d1, times, nonrewarded_times, names
 
 
+################################################################
+################################################################
+################################################################
+def load_data_ROIs_body_movements(root_dir,
+                                  animal_id,
+                                  session,
+                                  fname_whole_stack,
+                                  fname_blue_light):
+
+    '''
+    Function takes as input the whole stack locaNMF data, body movement trigger file name and blue-light shift
+
+    Exports:
+        stack: trigger centred segments of locaNMF data
+        stack_random: same as above but for random sampled trigger times
+        continuous:  locaNMF processed data shifted to match blue light shift
+        times: the trigger times;
+    '''
+
+
+    ############################################
+    ##### WHOLE STACK LOCANMF ##################
+    ############################################
+    dall = np.load(fname_whole_stack)
+
+    #
+    d1 = dall['whole_stack'].T
+
+    # zero out beginning and end, usually they are screwed up by excitation light
+    d1[:900]=d1[900]
+    d1[-900:]=d1[-900]
+
+    # this is the calcium excitation light, we need to shift the dataset to match it
+    if True:
+        blue = np.load(fname_blue_light)
+        start = blue['start_blue']
+        d1 = np.roll(d1,-start,axis=0)
+
+    ############################################
+    ##### BODY MOVEMENT TRIGGERS ###############
+    ############################################
+    fname_ = os.path.join(root_dir,
+                                animal_id,
+                                'tif_files',
+                                session,
+                                session+'_0secNoMove_movements.npz')
+
+    data = np.load(fname_, allow_pickle=True)
+
+    #
+    names = data['labels']
+
+    # load video shift peaks from left paw, right paw and lever DLC traces
+    shift = data['video_shift']
+    print (shift)
+
+    # load binarized body initiation matrices
+    inits = data['feature_initiations']
+    print (inits.shape)
+    print ("INITS: ", inits)
+
+    #
+    times = []
+    for k in range(inits.shape[0]):
+        idx = np.where(inits[k]==1)[0]
+        print (k, idx.shape)
+        print (idx)
+        idx = idx*2
+        print (idx)
+        times.append(idx)
+
+    ############################################
+    ######### FIGURE OUT SHIFT INDEX ###########
+    ############################################
+    # load the manual shift id from file to select one of the shifts from above
+    good_alignments = np.loadtxt(os.path.join(root_dir,
+                                             animal_id,
+                                             'tif_files',
+                                             'sessions_DLC_alignment_good.txt'),
+                                 dtype='str')
+    # print ("good alignmnets", good_alignments)
+    for k in range(len(good_alignments)):
+        temp = good_alignments[k][0].replace(',','')
+        if temp in session:
+            shift_id = int(good_alignments[k][1])
+            shift = shift[shift_id]
+            print ("Ideal DLC shift: ", shift )
+            break
+
+
+    ############################################
+    ##### BODY MOVEMENT TRIGGERS ###############
+    ############################################
+    # make a time triggered stack to visualize there is indeed a peak in the correct place
+    stack = []
+    stack_random = []
+    min_lockout = 30*3  # min 3 seconds away from each trigger
+
+    from tqdm import trange, tqdm
+    # loop over all body parts
+    window_width = 900
+    
+    for f in range(len(times)):
+        stack.append([])
+        stack_random.append([])
+
+        # loop over each time point
+        remove_ids = []  # keep track of which times cannot generate a proper ca stack
+        stack_temp = []
+        stack_random_temp = []
+        ctr2=0
+        for t in tqdm(times[f]):
+            temp = d1[t-window_width:t+window_width]
+            #print ("temp: ", temp.shape, t-window_width,"..",t+window_width)
+            if len(temp)==window_width*2:
+                stack_temp.append(temp)
+
+                # grab random segments also at the same time;
+                while True:
+                    t_random = np.random.randint(window_width,40000-window_width)
+                    min_diff = np.min(np.abs(times[f]-t_random))
+                    if min_diff>min_lockout:
+                        temp2 = d1[t_random-window_width:t_random+window_width]
+                        #print ("temp2: ", temp2.shape)
+                        if len(temp2)==window_width*2:
+                            #stack_random[f].append(temp2)
+                            stack_random_temp.append(temp2)
+                        break
+            else:
+                remove_ids.append(ctr2)
+            ctr2+=1
+
+        stack[f]= np.array(stack_temp)
+        stack_random[f] = np.array(stack_random_temp)
+
+        # delete all the times where calcium data couldn't be generated
+        if len(remove_ids)>0:
+            remove_ids = np.array(remove_ids)
+            tt = np.array(times[f])
+            tt = np.delete(tt, remove_ids)
+            times[f] = tt
+        #
+
+        print ("area: ", f, " ", stack[f].shape, stack_random[f].shape)
+
+    stack = np.array(stack)
+    stack_random = np.array(stack_random)
+    print ("stacK: ", stack.shape, stack[0].shape, ' random: ', stack_random.shape)
+    #
+    return stack, stack_random, d1, times, names
+#
+
+
+
+#
 def get_04_02_triggers(root_dir, recording):
 
     # make sure locs
@@ -220,7 +397,7 @@ def visualize_loaded_data(data_continuous,
 
   
 # 
-def load_rois_and_pca(root_dir, animal_id, session):
+def load_rois_lever_pulls(root_dir, animal_id, session):
     # ROI FILENAME
     fname_whole_stack_ROI = os.path.join(root_dir,
                                    animal_id, 
@@ -228,12 +405,12 @@ def load_rois_and_pca(root_dir, animal_id, session):
                                    session,
                                    session+'_locanmf_wholestack.npz')
 
-    # PCA FILENAME
-    fname_whole_stack_PCA = os.path.join(root_dir,
-                                    animal_id,
-                                    'tif_files',
-                                    session,
-                                    session + '_whole_stack_trial_ROItimeCourses_15sec_pca30components.npy')
+    # # PCA FILENAME
+    # fname_whole_stack_PCA = os.path.join(root_dir,
+    #                                 animal_id,
+    #                                 'tif_files',
+    #                                 session,
+    #                                 session + '_whole_stack_trial_ROItimeCourses_15sec_pca30components.npy')
 
     # same for both PCA and ROI
     fname_lever_times = os.path.join(root_dir,
@@ -251,6 +428,7 @@ def load_rois_and_pca(root_dir, animal_id, session):
     ########################################
     ######### LOAD PCA DATA ################
     ########################################
+    # NOT USED CURRENTLY FOR SUPER_session processing
     # data_segments, data_continuous, trigger_times, shift = load_data_PCA(fname_whole_stack_PCA,
     #                                                                      fname_lever_times,
     #                                                                      fname_blue_light)
@@ -264,21 +442,79 @@ def load_rois_and_pca(root_dir, animal_id, session):
      data_continuous, 
      rewarded_times,
      nonrewarded_times,
-     names) = load_data_ROIs(fname_whole_stack_ROI,
-                             fname_lever_times,
-                             fname_blue_light)
+     names) = load_data_ROIs_lever_pulls(fname_whole_stack_ROI,
+                                         fname_lever_times,
+                                         fname_blue_light)
 
     #
     data_continuous -= data_continuous.mean(0)
     
 	# get trial average
-    #data_segments = data_segments.mean(0)
-
 	# subtract mean to bring it to 0
-    # print ("Data segemnts before subtracgtin mean: ", data_segments.shape)
     for k in range(data_segments.shape[0]):
         data_segments[k] -= data_segments[k].mean(0)
         data_segments_random[k] -= data_segments_random[k].mean(0)
 
 
     return data_segments, data_segments_random, data_continuous, rewarded_times, nonrewarded_times, names
+
+
+#
+def load_rois_body_movements(root_dir, animal_id, session):
+    # ROI FILENAME
+    fname_whole_stack_ROI = os.path.join(root_dir,
+                                   animal_id,
+                                   'tif_files',
+                                   session,
+                                   session+'_locanmf_wholestack.npz')
+
+
+    # #
+    # fname_body_movements = os.path.join(root_dir,
+    #                                 animal_id,
+    #                                 'tif_files',
+    #                                 session,
+    #                                 session + '_0secNoMove_movements.npz')
+
+    #
+    fname_blue_light = os.path.join(root_dir,
+                                    animal_id,
+                                    'tif_files',
+                                    session,
+                                    'blue_light_frame_triggers.npz')
+
+    ########################################
+    ######### LOAD PCA DATA ################
+    ########################################
+    # NOT USED CURRENTLY FOR SUPER_session processing
+    # data_segments, data_continuous, trigger_times, shift = load_data_PCA(fname_whole_stack_PCA,
+    #                                                                      fname_lever_times,
+    #                                                                      fname_blue_light)
+
+
+    ########################################
+    ######### LOAD ROI DATA ################
+    ########################################
+    (data_segments,
+     data_segments_random,
+     data_continuous,
+     times,
+     names) = load_data_ROIs_body_movements(root_dir,
+                                            animal_id,
+                                            session,
+                                            fname_whole_stack_ROI,
+                                            fname_blue_light)
+
+    #
+    data_continuous -= data_continuous.mean(0)
+
+	# get trial average
+	# subtract mean to bring it to 0
+    # print ("Data segemnts before subtracgtin mean: ", data_segments.shape)
+    for f in range(len(data_segments)):
+        for k in range(len(data_segments[f])):
+            data_segments[f][k] -= data_segments[f][k].mean(0)
+            data_segments_random[f][k] -= data_segments_random[f][k].mean(0)
+
+
+    return data_segments, data_segments_random, data_continuous, times, names
